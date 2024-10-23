bd5ad46e94e9d751c1916aec6eda868e20dbfd56
Kosalvireak <virakvary@gmail.com>
2024-10-24 00:53:14 +0700

    feat: new string

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("androidx.core:core-ktx:+")
+    implementation ("com.squareup.picasso:picasso:2.71828")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
