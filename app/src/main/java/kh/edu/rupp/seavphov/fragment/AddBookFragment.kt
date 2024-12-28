1caac0bd5e39fa428fcc4a18992aeda7bc719df2
Kosalvireak <virakvary@gmail.com>
2024-12-28 14:32:33 +0700

    add progress bar to add book

--- app/src/main/java/kh/edu/rupp/seavphov/fragment/AddBookFragment.kt
+++ app/src/main/java/kh/edu/rupp/seavphov/fragment/AddBookFragment.kt
@@ -1,20 +1,41 @@
 package kh.edu.rupp.seavphov.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import kh.edu.rupp.seavphov.databinding.FragmentAddbooksBinding
+import kh.edu.rupp.seavphov.databinding.FragmentAddbookBinding
 
 class AddBookFragment : Fragment() {
-    private var binding: FragmentAddbooksBinding? = null
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentAddbooksBinding.inflate(inflater, container, false)
-        return binding!!.getRoot()
+    private lateinit var binding: FragmentAddbookBinding;
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddbookBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+    }
+
+    private fun showMainLoading() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.bodySection.visibility = View.GONE
+    }
+
+    private fun hideMainLoading() {
+        binding.progressBar.visibility = View.GONE
+        binding.bodySection.visibility = View.VISIBLE
+    }
+
+
+    @SuppressLint("SetTextI18n")
+    private fun showErrorContent() {
+        binding.bookTitle.text = "Something went wrong!";
     }
 }
