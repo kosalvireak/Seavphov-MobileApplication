f481950c42ed1abd1a034ecc4234ccd4793cd5ac
Kosalvireak <virakvary@gmail.com>
2025-01-04 16:56:26 +0700

    refactor api to support backend

--- app/src/main/java/kh/edu/rupp/seavphov/viewmodel/HomeFragmentViewModel.kt
+++ app/src/main/java/kh/edu/rupp/seavphov/viewmodel/HomeFragmentViewModel.kt
@@ -8,45 +8,39 @@ import androidx.lifecycle.viewModelScope
 import kh.edu.rupp.seavphov.api.ApiManager
 import kh.edu.rupp.seavphov.model.ApiState
 import kh.edu.rupp.seavphov.model.Book
-import kh.edu.rupp.seavphov.model.Carousel
 import kh.edu.rupp.seavphov.model.State
 import kotlinx.coroutines.launch
 
 class HomeFragmentViewModel : ViewModel() {
 
-//    readonly property
+    //    readonly property
 
     //    for Carousel
-    private val _homeState = MutableLiveData<ApiState<Carousel>>()
-    val homeState: LiveData<ApiState<Carousel>> get() = _homeState;
-//    for Carousel
+    private val _carouselState = MutableLiveData<ApiState<Book>>()
+    val carouselState: LiveData<ApiState<Book>> get() = _carouselState;
+    //    for Carousel
 
     //    for NewestAddition
     private val _newestAdditionState = MutableLiveData<ApiState<ArrayList<Book>>>()
     val newestAdditionState: LiveData<ApiState<ArrayList<Book>>> get() = _newestAdditionState;
-//    for NewestAddition
+    //    for NewestAddition
 
     //    for ThisWeekHighlight
     private val _thisWeekHighlightState = MutableLiveData<ApiState<ArrayList<Book>>>()
     val thisWeekHighlightState: LiveData<ApiState<ArrayList<Book>>> get() = _thisWeekHighlightState;
 //    for ThisWeekHighlight
 
-    //    for BooksList
-    private val _booksListState = MutableLiveData<ApiState<ArrayList<Book>>>()
-    val booksListState: LiveData<ApiState<ArrayList<Book>>> get() = _booksListState;
-
-    //    for BooksList
-    fun loadHome() {
-        val apiService = ApiManager.getApiService()
+    fun loadCarousel() {
+        val apiService = ApiManager.getSeavphovApiService()
         viewModelScope.launch {
-            val carouselResponse = apiService.loadCarousel()
+            val carouselResponse = apiService.loadBookById("67769f26b853ad8d58317f0d")
             try {
                 if (carouselResponse.isSuccess()) {
                     Log.d("Seavphov", "Load Carousel Success")
-                    _homeState.postValue(ApiState(State.success, carouselResponse.data))
+                    _carouselState.postValue(ApiState(State.success, carouselResponse.data))
                 } else {
                     Log.d("Seavphov", "Load Carousel Error")
-                    _homeState.postValue(ApiState(State.error, null))
+                    _carouselState.postValue(ApiState(State.error, null))
                 }
             } catch (ex: Exception) {
                 Log.e("Seavphov", "Error while loading profile: $ex")
@@ -75,9 +69,10 @@ class HomeFragmentViewModel : ViewModel() {
     }
 
     fun loadThisWeekHighlight() {
-        val apiService = ApiManager.getApiService()
+        val apiService = ApiManager.getSeavphovApiService()
         viewModelScope.launch {
-            val response = apiService.loadThisWeekHighlight()
+            val response = apiService.loadAllSeavphov()
+            Log.d("Seavphov", "Load ThisWeekHighlight Success $response")
             try {
                 if (response.isSuccess()) {
                     Log.d("Seavphov", "Load ThisWeekHighlight Success")
@@ -85,24 +80,6 @@ class HomeFragmentViewModel : ViewModel() {
                 } else {
                     Log.d("Seavphov", "Load ThisWeekHighlight Error")
                     _thisWeekHighlightState.postValue(ApiState(State.error, null))
-                }
-            } catch (ex: Exception) {
-                Log.e("Seavphov", "Error while loading ThisWeekHighlight: $ex")
-            }
-        }
-    }
-
-    fun loadBooksList() {
-        val apiService = ApiManager.getApiService()
-        viewModelScope.launch {
-            val response = apiService.loadBooksList()
-            try {
-                if (response.isSuccess()) {
-                    Log.d("Seavphov", "Load ThisWeekHighlight Success")
-                    _booksListState.postValue(ApiState(State.success, response.data))
-                } else {
-                    Log.d("Seavphov", "Load ThisWeekHighlight Error")
-                    _booksListState.postValue(ApiState(State.error, null))
                 }
             } catch (ex: Exception) {
                 Log.e("Seavphov", "Error while loading ThisWeekHighlight: $ex")
