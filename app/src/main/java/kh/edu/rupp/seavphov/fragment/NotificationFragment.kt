8ad455c9b4181f678309978e4b82bffdcddce607
Kosalvireak <virakvary@gmail.com>
2025-01-22 23:55:25 +0700

    improve UI notification

--- app/src/main/java/kh/edu/rupp/seavphov/fragment/NotificationFragment.kt
+++ app/src/main/java/kh/edu/rupp/seavphov/fragment/NotificationFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import kh.edu.rupp.seavphov.databinding.FragmentNotificationBinding
 import android.annotation.SuppressLint
+import android.widget.LinearLayout
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kh.edu.rupp.seavphov.adapter.NotificationListAdapter
 import kh.edu.rupp.seavphov.model.Notification
 import kh.edu.rupp.seavphov.model.State
@@ -47,19 +50,17 @@ class NotificationFragment : Fragment() {
     }
 
     private fun displayNotificationList(notificationList: ArrayList<Notification>) {
-        binding.notificationRecyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        binding.notificationRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         notificationListAdapter = NotificationListAdapter(notificationList);
         binding.notificationRecyclerView.adapter = notificationListAdapter
     }
 
     private fun showMainLoading() {
         binding.progressBar.visibility = View.VISIBLE
-        binding.notificationHeader.visibility = View.GONE
     }
 
     private fun hideMainLoading() {
         binding.progressBar.visibility = View.GONE
-        binding.notificationHeader.visibility = View.VISIBLE
     }
 
 
