c7788d2944b414718b20ee15cb786d382f63794d
Kosalvireak <virakvary@gmail.com>
2025-01-23 23:58:15 +0700

    finalize configuration uat and production

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,7 +26,8 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file("/Users/zekk/desktop/Seavphov.jks")
+            storeFile =
+                file("./Seavphov.jks")
             storePassword = "123456789"
             keyAlias = "Seavphov"
             keyPassword = "123456789"
@@ -36,20 +37,27 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("release")
-            isMinifyEnabled = true
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = true // make our code smaller, prevent de-compile
+            proguardFiles( // config file for minify
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
     flavorDimensions += "Seavphov"
-    productFlavors{
+    productFlavors {
         create("uat") {
             dimension = "Seavphov"
             applicationId = "kh.edu.rupp.seavphov.uat"
-            resValue("String", "app_name" , "Seavphov UAT")
+            resValue("string", "app_name", "Seavphov UAT")
 
 //            As we have one source of api, im just gonna put it the same one
-            buildConfigField("String", "apiBaseUrl", "\"https://raw.githubusercontent.com/kosalvireak/Seavphov-MobileApplication/master/dummy/\"")
+            buildConfigField(
+                "String",
+                "apiBaseUrl",
+                "\"https://raw.githubusercontent.com/kosalvireak/Seavphov-MobileApplication/master/dummy/\""
+            )
         }
 //        create("beta") {
 //            dimension = "Seavphov"
@@ -61,11 +69,15 @@ android {
 //        }
         create("prd") {
             dimension = "Seavphov"
-            applicationId = "kh.edu.rupp.seavphov.prd"
-            resValue("String", "app_name" , "Seavphov")
+            applicationId = "kh.edu.rupp.seavphov"
+            resValue("string", "app_name", "Seavphov PRD")
 
 //            As we have one source of api, im just gonna put it the same one
-            buildConfigField("String", "apiBaseUrl", "\"https://seavphov-mobileapplication.onrender.com/api/\"")
+            buildConfigField(
+                "String",
+                "apiBaseUrl",
+                "\"https://seavphov-mobileapplication.onrender.com/api/\""
+            )
         }
     }
 
@@ -86,20 +98,20 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation("androidx.core:core-ktx:+")
-    implementation ("com.squareup.picasso:picasso:2.71828")
-    implementation ("androidx.activity:activity-ktx:1.6.1")
-    implementation ("androidx.fragment:fragment-ktx:1.6.1")
-    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.2.0")
-    implementation ("androidx.core:core-ktx:1.12.0")
-    implementation ("androidx.compose.material3:material3:1.3.1")
-    implementation ("com.google.android.material:material:1.12.0")
-    implementation ("androidx.navigation:navigation-runtime-ktx:2.8.4")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.8.4")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.8.4")
-    implementation ("com.github.bumptech.glide:glide:4.11.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.11.0")
-    implementation ("com.google.firebase:firebase-storage:21.0.1")
+    implementation("com.squareup.picasso:picasso:2.71828")
+    implementation("androidx.activity:activity-ktx:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.1")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.2.0")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.compose.material3:material3:1.3.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.navigation:navigation-runtime-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.11.0")
+    implementation("com.google.firebase:firebase-storage:21.0.1")
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
