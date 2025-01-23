ca37f4bc039f94072a9b3e1ff451f87c18da8d88
Kosalvireak <virakvary@gmail.com>
2025-01-23 21:00:01 +0700

    fix gradle project level

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.4")
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
